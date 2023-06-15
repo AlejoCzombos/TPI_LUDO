87c29fdcbfde87ef2e7653ae17bf847381765f2b
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2023-06-14 21:05:08 -0300

    Clase Jugador

---
+++ TPI Programacion - Ludo/Jugador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPI_Programacion___Ludo
+{
+    internal class Jugador
+    {
+        public Jugador(Enum colores) 
+        {
+
+        }
+
+        private Ficha[] fichas = new Ficha[4];
+
+        //private enum color = 
+
+        private Recorrido recorrido = new Recorrido();
+
+        public Ficha[] Fichas { get => fichas; }
+
+        //private RecorridoFinal recorridoFinal = new RecorridoFinal();
+
+        public void moverFicha(int indiceFicha, int pasos)
+        {
+            //recorrido.ProximaPosicion();
+        }
+    }
+}
